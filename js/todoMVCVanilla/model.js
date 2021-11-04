fddbe07997b0f33fb0873dc7bc4a621be64a3e5b
eunsunShin <unusual0218@gmail.com>
2021-11-04 21:51:07 +0900

    Add: model class 작성

--- js/todoMVCVanilla/model.js
+++ js/todoMVCVanilla/model.js
@@ -1,15 +1,16 @@
 class Item {
   constructor(value) {
-    this.id = `item${model.list.length}`;
+    this.id = model.list.length > 0 ? model.list.length + 1 : 1;
     this.content = value;
     this.checked = false;
   }
 }
+
 class Model {
   constructor() {
     this.list = [
-      { content: '세수하기', checked: false },
-      { content: '이 닦기', checked: false },
+      { id: 1, content: '세수하기', checked: false },
+      { id: 2, content: '이 닦기', checked: false },
     ]
   }
 
@@ -18,16 +19,21 @@ class Model {
     this.list.push(newItem);
   }
 
-  removeItemData(itemIndex) {
-    this.list.splice(itemIndex, 1);
+  editItemData(id, updatedText) {
+    this.list = this.list.map((item) =>
+      item.id === id ? { id: item.id, content: updatedText, checked: item.checked } : item
+    )
   }
 
-  editItemData(item, itemIndex) {
-    this.splice(itemIndex, 1, item);
+  deleteItemData(id) {
+    this.list = this.list.filter((item) => item.id !== id);
   }
 
-  checkItem(itemIndex) {
-    this.list[itemIndex].checked = true;
+  toggleItem(id) {
+    this.list[id].checked = !this.list[id].checked
+    // this.list = this.list.map((item) =>
+    //   item.id === id ? { id: item.id, content: item.content, checked: !item.checked } : item
+    // )
   }
 }
 

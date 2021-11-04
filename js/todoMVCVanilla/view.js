9e1be1d413b11c3dc74f6bb937a9950e5a148b7b
eunsunShin <unusual0218@gmail.com>
2021-11-04 23:44:23 +0900

    Add: class view 작성

--- js/todoMVCVanilla/view.js
+++ js/todoMVCVanilla/view.js
@@ -1,58 +1,93 @@
-
-
 const makeItemElement = (item) => {
   return `<li>
   <input type="checkbox" />
   ${item.content}
   <button>삭제</button>
-</li>`
-}
+</li>`;
+};
 
 class View {
   constructor() {
-    this.toDoList = document.getElementById("toDoList");
+    this.app = this.getElement("#root");
+
+    this.title = this.createElement("h1");
+    this.title.textContent = "Todos";
+
+    this.form = this.createElement("form");
+
+    this.input = this.createElement("input");
+    this.input.type = "text";
+    this.input.placeholder = "add Todo";
+    this.input.name = "todo";
+
+    this.submitButton = this.createElement("button");
+    this.submitButton.textContent = "추가";
+
+    this.todoList = this.createElement("ul", "to_do_list");
+
+    this.form.append(this.input, this.submitButton);
+
+    this.app.append(this.title, this.form, this.todoList);
   }
 
-  
-  renderList(list) {
-    // const toDoList = document.getElementById("toDoList");
-    const listItems = list.map((item) => makeItemElement(item));
-    
-    const listComponent = listItems.reduce((prev, curr) => prev + curr, '');
-    toDoList.innerHTML = listComponent;
-    
-    // const deleteButtons = document.getElementsByClassName('delete_button');
-    
-    // for (let i = 0; i < deleteButtons.length; i++) {
-    //   deleteButtons[i].addEventListener('click', controller.deleteItem);
-    // }
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) element.className.add(className);
+
+    return element;
   }
-  
-    makeItemElement(item) {
-      return `<li>
-    <input type="checkbox" />
-    ${item.content}
-    <button id="${item.id}" class="delete_button">삭제</button>
-  </li>`
+
+  getElement(selector) {
+    const element = document.querySelector(selector);
+
+    return element;
+  }
+
+  get _todoText() {
+    return this.input.value;
+  }
+
+  _resetInput() {
+    this.input.value;
+  }
+
+  renderTodos(todos) {
+    while (this.todoList.firstChild) {
+      this.todoList.removeChild(this.todoList.firstChild);
     }
 
-  getInputValue() {
-    const input = document.querySelector('input');
-    if (input.value === '') return;
-    return input.value;
-  }
+    if (todos.length === 0) {
+      const p = this.createElement("p");
+      p.textContent = "Nothing to do! Add a task?";
+      this.todoList.append(p);
+    } else {
+      todos.foreach((todo) => {
+        const li = this.createElement("li");
+        li.id = todo.id;
 
-  addItem(item) {
-    let newItem = `<li>
-    <input type="checkbox" />
-    ${item.content}
-    <button id="${item.id}" class="delete_button">삭제</button>
-  </li>`;
-    toDoList.appendChild(newItem);
-  }
+        const checkbox = this.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.checked = todo.checked;
 
-  deleteItem() {
+        const span = this.createElement('span');
+        span.contentEditable = true;
+        span.className.add('editable')
 
+        if (todo.checked) {
+          const strike = this.createElement('s');
+          strike.textContent = todo.text;
+          span.append(strike)
+        } else {
+          span.textContent = todo.content;
+        }
+
+        const deleteButton = this.createElement('button', 'delete');
+        deleteButton.textContent = '삭제';
+        li.append(checkbox, span, deleteButton)
+
+        this.todoList.append(li)
+      });
+    }
   }
 }
 
